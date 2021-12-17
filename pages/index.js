825dd280e169b268b43573bd345b60baab0eee08
Shobhit Jain <71167346+Shobhits7@users.noreply.github.com>
2021-12-17 23:40:30 +0530

    eh

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,8 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
+
+        <!--Responsive Viewport Meta tag-->
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
           <meta property="og:title" content="Mavericks">
             <meta property="og:site_name" content="Mave OP">
@@ -13,21 +15,20 @@ export default function Home() {
                 <meta name="description" content="REAL MAVE IS BACK!!">
                   <meta property="theme-color" content="#B81217">
                     <meta property="og:image" content="logo.png">
-                      <meta property="og:url" content="https://mavericks.cf">
-                        <!--Responsive Viewport Meta tag-->
-                        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
-                          <title>Mavericks</title>
-                          <link rel="https://cdn.discordapp.com/icons/919827956795338812/a_5b12f0e6aaa8639230bb7747c783ae42.gif?size=1024" href="/favicon.ico" />
-                        </Head>
+                      <meta property="og:url" content="https://mavericks.cf"></meta>
+                      <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+                        <title>Mavericks</title>
+                        <link rel="https://cdn.discordapp.com/icons/919827956795338812/a_5b12f0e6aaa8639230bb7747c783ae42.gif?size=1024" href="/favicon.ico" />
+                      </Head>
 
-                        <main>
-                          <Header title="Welcome to Mavericks" />
-                          <p className="description">
-                            <code>REAL MAVE IS BACK WITH REAL OWNERS</code>
-                          </p>
-                        </main>
+                      <main>
+                        <Header title="Welcome to Mavericks" />
+                        <p className="description">
+                          <code>REAL MAVE IS BACK WITH REAL OWNERS</code>
+                        </p>
+                      </main>
 
-                        <Footer />
-                      </div>
-                      )
+                      <Footer />
+                    </div>
+                    )
 }
