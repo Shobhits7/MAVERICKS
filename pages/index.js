6403c8b36543f62830bc1bd73f914b95121691b9
Shobhit Jain <71167346+Shobhits7@users.noreply.github.com>
2021-12-17 23:19:45 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Mavericks</title>
+        <link rel="https://cdn.discordapp.com/icons/919827956795338812/a_5b12f0e6aaa8639230bb7747c783ae42.gif?size=1024" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to Mavericks" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          <code>REAL MAVE IS BACK WITH REAL OWNERS</code>
         </p>
       </main>
 
